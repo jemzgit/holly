d1550a475b65be8b3b7d7abf3fbbaa692cc28a7e
jjemz <jemzdotorg@gmail.com>
2017-03-21 13:15:33 +0700

    update contact

--- src/SectionContact.js
+++ src/SectionContact.js
@@ -22,7 +22,8 @@ const SectionContact= () => (
         <GrApp> 
             <Heading style={{paddingTop: '20px'}}>Contact Information</Heading>
             <Paragraph>
-            If you have any questions or suggestions, please let us know. We would be always glad to help you :) Here is various ways that you can contact us:
+            If you have any questions or suggestions, please let us know. We would be always glad to help you :) Here is various ways that you can contact us.
+            If you or the taxi driver needs to talk to someone in Thai langauge, please contact Kukkik first:
             </Paragraph>
         </GrApp>
 
